79019b9b324df457a283cc59d0c8de4696737c60
todd <git@toddmerrill.com>
2016-05-19 20:06:39 +0200

    node workshop 'learnyounode' - my first async io - changed read call

---
+++ src/myfirstAsyncIo.js
@@ -0,0 +1,13 @@
+var projDir = 'D:/dev/src/firstnode/src/';
+var fileName = 'myFirstAsyncIo.js';
+var fs = require('fs');
+
+var callback = function (err, code) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('this file:\n\n' + code + '\nlines: ' + code.split('\n').length);
+    }
+}
+
+fs.readFile(projDir + fileName, 'utf-8', callback);
