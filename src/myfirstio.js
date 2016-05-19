e18479e6717c9f8f8e86f3be7517ff77212ff50d
todd <git@toddmerrill.com>
2016-05-19 20:00:04 +0200

    node workshop 'learnyounode' - my first async io

--- src/myfirstio.js
+++ src/myfirstio.js
@@ -1,7 +1,7 @@
 var projDir = 'D:/dev/src/firstnode/src/';
 
 var fs = require('fs');
-var buf = fs.readFileSync(projDir + 'myfirstio.js')
+var buf = fs.readFileSync(projDir + 'myFirstIo.js')
 var code = buf.toString();
 console.log('this file:\n\n' + code + 'lines: ' + code.split('\n').length);
 
