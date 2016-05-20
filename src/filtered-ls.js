f028c217ee85e83b6ded318beeae38a2f8c6eb7c
todd <git@toddmerrill.com>
2016-05-20 10:46:43 +0200

    converted loop to forEach

--- src/filtered-ls.js
+++ src/filtered-ls.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 
-var directory = process.argv[2]
-var ext = process.argv[3]
+var directory = process.argv[2];
+var ext = process.argv[3];
 
 console.log('dir: ' + directory + '  ext: ' + ext);
 fs.readdir(directory, function (err, data) {
-    for (key in data) {
-        if (data[key].endsWith(ext)) {
-            console.log(data[key])
+    data.forEach(function(item) {
+        if (item.endsWith(ext)) {
+            console.log(item)
         }
-    }
-})
+    });
+});
