fc79ba0d17ae7998e554901948c4854cb9d48689
todd <git@toddmerrill.com>
2016-05-25 19:49:56 +0200

    workshopper learnyounode: make it modular - corrected error handling

--- src/make-it-modular/filtered-ls-callback.js
+++ src/make-it-modular/filtered-ls-callback.js
@@ -3,11 +3,14 @@ var ls = require('./filtered-ls-module');
 var directory = process.argv[2];
 var ext = process.argv[3];
 
-console.log('dir: ' + directory + '  ext: ' + ext + '\n\n');
+//console.log('dir: ' + directory + '  ext: ' + ext + '\n\n');
 
 ls(directory, ext, function (err, data) {
+    if (err) {
+        return console.error(err)
+    }
     data.forEach(function (item) {
         console.log(item);
     })
-})
+});
 
--- src/make-it-modular/filtered-ls-module.js
+++ src/make-it-modular/filtered-ls-module.js
@@ -1,13 +1,17 @@
-module.exports = function(directory,ext,callback) {
+module.exports = function (directory, ext, callback) {
     var fs = require('fs');
 
     fs.readdir(directory, function (err, data) {
+        if (err) {
+            return callback(err);
+        }
         var result = [];
         for (var value of data) {
-            if (value.endsWith(ext)) {
+            if (value.endsWith('.' + ext)) {
                 result.push(value);
             }
         }
         callback(null, result);
     })
+
 };
