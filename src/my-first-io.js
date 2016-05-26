59a9b639361b1906e9fc54c2e50936c79e13198b
todd <git@toddmerrill.com>
2016-05-26 10:38:48 +0200

    workshopper learnyounode: my first i/o

--- src/my-first-io.js
+++ src/my-first-io.js
@@ -1,6 +1,7 @@
-var projDir = 'D:/dev/src/firstnode/src/';
+var file = process.argv[2];
 
 var fs = require('fs');
-var buf = fs.readFileSync(projDir + 'my-first-io.js')
+var buf = fs.readFileSync(file);
 var code = buf.toString();
-console.log('this file:\n\n' + code + 'lines: ' + code.split('\n').length);
+//console.log('this file:\n\n' + code + 'lines: ' + code.split('\n').length);
+console.log(code.split('\n').length - 1);
