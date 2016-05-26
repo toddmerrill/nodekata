b5c06cd25afd48063daaf1a84338d5337cb37591
todd <git@toddmerrill.com>
2016-05-26 10:46:46 +0200

    workshopper learnyounode: my first async i/o

--- src/my-first-async-io.js
+++ src/my-first-async-io.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 
+
 var callback = function (err, code) {
     if (err) {
         return console.log(err);
     }
     console.log(code.split('\n').length - 1);
-}
+};
 
 fs.readFile(process.argv[2], 'utf-8', callback);
