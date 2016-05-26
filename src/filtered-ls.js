3dec8d0e9452e71e5a67851be4fb49400faebdf0
todd <git@toddmerrill.com>
2016-05-26 10:50:10 +0200

    workshopper learnyounode: filtered ls

--- src/filtered-ls.js
+++ src/filtered-ls.js
@@ -3,10 +3,10 @@ var fs = require('fs');
 var directory = process.argv[2];
 var ext = process.argv[3];
 
-console.log('dir: ' + directory + '  ext: ' + ext);
+//console.log('dir: ' + directory + '  ext: ' + ext);
 fs.readdir(directory, function (err, data) {
     data.forEach(function(item) {
-        if (item.endsWith(ext)) {
+        if (item.endsWith('.'+ext)) {
             console.log(item)
         }
     });
