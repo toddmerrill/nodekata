6ab0cb4a62d53e80a9d5c4a4fb8539daa4188a65
todd <git@toddmerrill.com>
2016-05-28 14:00:17 +0200

    node.js by example - a simple module example

---
+++ src/nodejsbyexample/simple-module-call.js
@@ -0,0 +1,4 @@
+// script.js
+var book = require('./simple-module.js');
+console.log('Name: ' + book.name);
+book.read();
---
+++ src/nodejsbyexample/simple-module.js
@@ -0,0 +1,5 @@
+// book.js
+exports.name = 'Node.js by example';
+exports.read = function() {
+    console.log('I am reading ' + exports.name);
+}
