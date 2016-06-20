3fb02b9234331843c2b2d0995e421920eafcdc88
todd <git@toddmerrill.com>
2016-06-20 20:45:11 +0200

    workshopper functional-javascript: blocking event loop.  Minor test update

--- test/functionaljavascript/blocking-event-loop-test.js
+++ test/functionaljavascript/blocking-event-loop-test.js
@@ -5,6 +5,5 @@ var repeat =  require('../../src/functionaljavascript/blocking-event-loop.js');
 describe('blocking event loop', function () {
     it('repeat non blocking', function () {
         repeat(function() {console.log('repeating')},5);
-        //assert.deepEqual(output,[2, 4, 6, 8, 10],'array values should be doubled')
     });
 });
