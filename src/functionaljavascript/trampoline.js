90d1be097d1de570700665d5d0fe765fe73946b1
todd <git@toddmerrill.com>
2016-06-21 16:28:42 +0200

    workshopper functional-javascript: trampoline - Lots of reading for this one, but I understand now

--- src/functionaljavascript/trampoline.js
+++ src/functionaljavascript/trampoline.js
@@ -2,14 +2,20 @@ function repeat(operation, num) {
     // Modify this so it doesn't cause a stack overflow!
     if (num <= 0) return
     operation()
-    return repeat(operation, --num)
+    return function(){return repeat(operation, --num);}
 }
 
 function trampoline(fn) {
-    // You probably want to implement a trampoline!
+    return function(){
+        var res = fn.apply(this, arguments);
+        while(res instanceof Function){
+            res = res();
+        }
+        return res;
+    }
 }
 
 module.exports = function(operation, num) {
     // You probably want to call your trampoline here!
-    return repeat(operation, num)
+    return trampoline(repeat)(operation, num);
 }
