1846f8247a57970fbc0789958406375c42b9580e
todd <git@toddmerrill.com>
2016-06-23 20:58:29 +0200

    workshopper functional-javascript: async loops.  Updated test, but still confused.

--- test/functionaljavascript/async-loops-test.js
+++ test/functionaljavascript/async-loops-test.js
@@ -2,15 +2,17 @@ var assert = require('chai').assert;
 
 var loadUsers =  require('../../src/functionaljavascript/async-loops.js');
 
-var loader = function (id, callback) {
-    console.log('id:' + id);
+var loader = function (id) {
     return 'bla:'+id;
 };
 
 describe('load users', function () {
     it('loads users in the right order', function () {
         var bigArray = Array.from(Array(10000).keys())
-        loadUsers(bigArray, loader,
-        function(users){console.log('done:'+users.length)});
+        var users = loadUsers(bigArray, loader,
+        function(users){console.log('done:'+users.length);
+                        return users;});
+        assert.equal(10000, users.length)
+        assert.equal('bla:5000', users[5000]);
     });
 });
