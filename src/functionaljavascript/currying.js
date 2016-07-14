6a14d8f30309f74b086344ccab101d8a934ad6fc
todd <git@toddmerrill.com>
2016-07-14 10:06:15 +0200

    workshopper functional-javascript: currying.  Unfinished solution

--- src/functionaljavascript/currying.js
+++ src/functionaljavascript/currying.js
@@ -1,5 +1,37 @@
 function curryN(fn, n) {
-    // SOLUTION GOES HERE
+    function doCurry(arg) {
+
+    };
+    return doCurry;
+
+    var n  = n ? n : arguments.length;
+    var args = args || [];
+    var count = 0;
+    if (n == 0) {
+        return fn.apply(fn,args);
+    } else {
+        args[count] = arg
+        return curryN(arg, count++);
+    }
+
+    return function getCurry(arg, n) {
+
+        return getCurry(count++)
+    }()
+
+    return n == 0 ? fn() : curryN(fn, --n);
+    var args = [];
+    var current = 0;
+    return function (arg1) {
+        return function (arg2) {
+            return function (arg3) {
+                //console.log(args[0] + ":" + args[1] + ":" + args[2])
+                return fn(arg1, arg2, arg3);
+            };
+        };
+    }
+
+    return arguments.length === 0 ? fn()
 }
 
 module.exports = curryN
--- test/functionaljavascript/currying-test.js
+++ test/functionaljavascript/currying-test.js
@@ -8,9 +8,9 @@ describe('curry N params', function () {
             return one + two + three
         }
 
-        var curryC = curryN(add3)
-        var curryB = curryC(1)
-        var curryA = curryB(2)
+        var curryC = curryN(add3);
+        var curryB = curryC(1);
+        var curryA = curryB(2);
         assert.equal(curryA(3), 6);
         assert.equal(curryA(10), 13);
         assert.equal(curryN(add3)(1)(2)(3), 6);
