5bf7f63b95bb195e5d2cdaa0d09b8ba4f26b768a
todd <git@toddmerrill.com>
2016-07-14 11:30:49 +0200

    workshopper functional-javascript: currying.  I can't imagine being able to intuitively have this solution pop into my head.  As a daily kata it would still be unsolvable for each small variation for me.

--- src/functionaljavascript/currying.js
+++ src/functionaljavascript/currying.js
@@ -1,37 +1,12 @@
 function curryN(fn, n) {
-    function doCurry(arg) {
-
-    };
-    return doCurry;
-
-    var n  = n ? n : arguments.length;
-    var args = args || [];
-    var count = 0;
+    var n = n || fn.length;
     if (n == 0) {
-        return fn.apply(fn,args);
+        return fn();
     } else {
-        args[count] = arg
-        return curryN(arg, count++);
+        return function(arg) {
+            return curryN(fn.bind(fn, arg), n-1);
+        }
     }
-
-    return function getCurry(arg, n) {
-
-        return getCurry(count++)
-    }()
-
-    return n == 0 ? fn() : curryN(fn, --n);
-    var args = [];
-    var current = 0;
-    return function (arg1) {
-        return function (arg2) {
-            return function (arg3) {
-                //console.log(args[0] + ":" + args[1] + ":" + args[2])
-                return fn(arg1, arg2, arg3);
-            };
-        };
-    }
-
-    return arguments.length === 0 ? fn()
 }
 
 module.exports = curryN
--- test/functionaljavascript/currying-test.js
+++ test/functionaljavascript/currying-test.js
@@ -4,15 +4,18 @@ var curryN = require('../../src/functionaljavascript/currying');
 
 describe('curry N params', function () {
     it('adds three numbers', function () {
+        assert.equal(curryN(function(){return 5;}), 5);
+        assert.equal(curryN(function(one){return one;})(7), 7);
+
         function add3(one, two, three) {
             return one + two + three
         }
-
         var curryC = curryN(add3);
         var curryB = curryC(1);
         var curryA = curryB(2);
         assert.equal(curryA(3), 6);
         assert.equal(curryA(10), 13);
         assert.equal(curryN(add3)(1)(2)(3), 6);
+
     });
 });
