7fd1768588e0d0aeff801222a6ebb61e778df6a1
todd <git@toddmerrill.com>
2016-08-03 10:26:56 +0200

    workshopper functional-javascript: basic: every some.  Rewrote as kata.

--- src/functionaljavascript/basic-every-some.js
+++ src/functionaljavascript/basic-every-some.js
@@ -1,20 +1,11 @@
 function checkUsersValid(goodUsers) {
     return function allUsersValid(submittedUsers) {
-        return submittedUsers.every(function(submittedUser){
-            return goodUsers.some(function(goodUser){
-                return submittedUser.id === goodUser.id;
-            })
-        })
+        return submittedUsers.every(function(subElement) {
+            return goodUsers.some(function(element) {
+                return subElement.id === element.id;
+            });
+        });
     };
 }
 
 module.exports = checkUsersValid;
-
-/*
-var goodUsers = [{id:1},{id:2},{id:3}];
-
-var checkValid = checkUsersValid(goodUsers);
-
-console.log('valid1?:' + checkValid([{id:1},{id:3}]));
-console.log('\n\nvalid2?:' + checkValid([{id:2},{id:4}]));
-*/
